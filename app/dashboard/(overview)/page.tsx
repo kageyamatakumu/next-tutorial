d11a1eef2253bbd19bf533e4c68662567013b848
takumu <takumu155695@gmail.com>
2025-11-19 23:59:47 +0900

    seventh commit

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 
 export default async function Page() {
